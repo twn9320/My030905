19eec3d34f8794566d4b43ef613abc040155f1ef
twn9320 <twn9320@gmail.com>
2017-03-09 14:41:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/my030905/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.yvtc.my030905;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView wv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wv = (WebView)findViewById(R.id.webView);
+        wv.setWebViewClient(new WebViewClient());
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadUrl("http://www.hinet.net");
+    }
+}
