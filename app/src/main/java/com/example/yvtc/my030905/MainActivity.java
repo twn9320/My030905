31bf14caba6bab50145ff1da2c2b0b299a4a37b1
twn9320 <twn9320@gmail.com>
2017-03-09 16:27:18 +0800

    解決上一頁案件問題

--- app/src/main/java/com/example/yvtc/my030905/MainActivity.java
+++ app/src/main/java/com/example/yvtc/my030905/MainActivity.java
@@ -17,4 +17,15 @@ public class MainActivity extends AppCompatActivity {
         wv.getSettings().setJavaScriptEnabled(true);
         wv.loadUrl("file:///android_asset/index.html");
     }
+
+    @Override
+    public void onBackPressed() {
+        if (wv.getUrl().contains("index.html")){
+            super.onBackPressed();
+        }
+        else{
+            wv.goBack();
+        }
+
+    }
 }
